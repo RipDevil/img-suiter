75fd275e4298075107fc27c9e16cd327a410af7e
MarkAnn <tiberi64@yandex.ru>
2021-01-02 21:27:27 +0700

    :books: Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# img-suiter
+# img-suiter (just a convenient app for myself)
+
+Moves files in subdirs into a ./photos dir in 1..n arranging
+
+Also converts .heic files
+
+npm i
+create a photos dir
+place folders in the root
+npm start
---
+++ index.js
@@ -0,0 +1,47 @@
+const root = './';
+const copyTo = './photos/';
+const fs = require('fs');
+const convert = require('heic-convert');
+const path = require('path');
+
+
+let i = 1;
+console.log('<=== Processing has started ===>');
+fs.readdirSync(root).forEach((file) => {
+  if (
+    file !== 'iter_renamer.js' &&
+    file !== 'photos' &&
+    file !== 'package.json' &&
+    file !== 'package-lock.json' &&
+    file !== 'node_modules'
+  ) {
+    fs.readdirSync(root + file).forEach(async (p) => {
+      try {
+        if (!p.includes('.zip')) {
+          const baseName = p.split('.')[0];
+          const filePath = root + file + '/' + baseName;
+          const extName = path.extname(root + file + '/' + p).toLowerCase();
+
+          console.log(`${i} --- ${baseName} is being processed...`)
+          if (p.match(/.heic/i)) {
+            const inputBuffer = fs.readFileSync(filePath + extName);
+            const savedIdx = i;
+            convert({
+              buffer: inputBuffer, // the HEIC file buffer
+              format: 'JPEG', // output format
+              quality: 1, // the jpeg compression quality, between 0 and 1
+            }).then((data) => {
+              fs.writeFileSync(copyTo + savedIdx + '.jpg', data);
+            });
+          } else {
+            fs.copyFileSync(filePath + extName, copyTo + i + extName);
+          }
+        }
+      } catch (err) {
+        console.error(err);
+      }
+
+      i++;
+    });
+  }
+});

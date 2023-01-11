01f750cf4e5eda679e4bb8ff40664a5c2a6d8081
RipDevil <tiberi64@yandex.ru>
2023-01-12 02:13:45 +0700

    small upd

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
 const root = './';
 const copyTo = './photos/';
 const fs = require('fs');
-const convert = require('heic-convert');
 const path = require('path');
 
+const ignore = [
+  'iter_renamer.js',
+  'photos',
+  'package.json',
+  'package-lock.json',
+  'node_modules',
+  '.git',
+  '.gitignore',
+  'README.md',
+  'index.js',
+];
 
 let i = 1;
 console.log('<=== Processing has started ===>');
 fs.readdirSync(root).forEach((file) => {
-  if (
-    file !== 'iter_renamer.js' &&
-    file !== 'photos' &&
-    file !== 'package.json' &&
-    file !== 'package-lock.json' &&
-    file !== 'node_modules' &&
-    file !== '.git' &&
-    file !== 'index.js'
-  ) {
+  if (ignore.includes(file)) {
     fs.readdirSync(root + file).forEach(async (p) => {
       try {
         if (!p.includes('.zip')) {
@@ -24,23 +26,8 @@ fs.readdirSync(root).forEach((file) => {
           const filePath = root + file + '/' + baseName;
           const extName = path.extname(root + file + '/' + p).toLowerCase();
 
-          console.log(`${i} --- ${baseName} is being processed...`)
+          console.log(`${i} --- ${baseName} is being processed...`);
           fs.copyFileSync(filePath + extName, copyTo + i + extName);
-
-          // TODO: Needs optimisation for .heic -> .jpg convert
-          // if (p.match(/.heic/i)) {
-          //   const inputBuffer = fs.readFileSync(filePath + extName);
-          //   const savedIdx = i;
-          //   convert({
-          //     buffer: inputBuffer, // the HEIC file buffer
-          //     format: 'JPEG', // output format
-          //     quality: 1, // the jpeg compression quality, between 0 and 1
-          //   }).then((data) => {
-          //     fs.writeFileSync(copyTo + savedIdx + '.jpg', data);
-          //   });
-          // } else {
-          //   fs.copyFileSync(filePath + extName, copyTo + i + extName);
-          // }
         }
       } catch (err) {
         console.error(err);
